b0325a05d8bbc0dd291db7079d9320222ef29489
Max1330602 <1330602@cstj.qc.ca>
2020-11-11 11:06:59 -0500

    navigation

--- app/src/main/java/ca/qc/cstj/yannick_brayapp/MainActivity.kt
+++ app/src/main/java/ca/qc/cstj/yannick_brayapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package ca.qc.cstj.yannick_brayapp
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.branchesFragment, R.id.detailsFragment))
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        bnvNavigation.setupWithNavController(navController)
     }
 }
